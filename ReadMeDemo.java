1893090304f92ad3b75415db0b0d819e36885966
lucasbarros <ldebarros6162@students.mc3.edu>
2022-04-15 22:45:51 -0400

    DemoJavaProgram
    Simple java demo code that runs a for loop and increments the amount of money by 1 with every loop.

---
+++ ReadMeDemo.java
@@ -0,0 +1,8 @@
+public class ReadMeDemo{
+   public static void main(String[] args){
+      
+      for(int i = 0;i<75;i++){
+         System.out.println("Hello GitHub! I have $" + i);
+      }
+   }
+}
